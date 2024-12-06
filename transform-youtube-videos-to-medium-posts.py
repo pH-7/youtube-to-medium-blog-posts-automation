dc6c3f16a23f21e223aaa0b877aa0ef05bb64d62
Pierre-Henry Soria <pierre@ph7.me>
2024-12-06 18:47:47 +1100

    Avoid having "Effortless" in text

--- transform-youtube-videos-to-medium-posts.py
+++ transform-youtube-videos-to-medium-posts.py
@@ -268,7 +268,7 @@ def generate_article_from_transcript(transcript: str, title: str, source_languag
     Transcript: {transcript[:12000]}
 
     Structured as a Medium.com article in English while keeping the exact same voice tone as in the original transcript.
-    Use simple words, and DO NOT use any irrelevant or complicated adjective such as: Unlock, Explore, Insights, Today's Digital World, In today's world, Dive into, Refine, Evolving, Embrace, Embracing, Embark, Enrich, Envision, Unleash, Unmask, Unveil, Streamline, Fast-paced, Delve, Digital Age, Game-changer, Indulge, Merely, Endure.
+    Use simple words, and DO NOT use any irrelevant or complicated adjective such as: Unlock, Effortless, Explore, Insights, Today's Digital World, In today's world, Dive into, Refine, Evolving, Embrace, Embracing, Embark, Enrich, Envision, Unleash, Unmask, Unveil, Streamline, Fast-paced, Delve, Digital Age, Game-changer, Indulge, Merely, Endure.
     Use Markdown format for headings, links, bold, italic, etc:""",
 
         'fr': f"""{{instruction}} en supprimant les sons de remplissage comme "euh...", "bah", "ben", "hein" et autres tics verbaux similaires.
@@ -440,7 +440,7 @@ def generate_article_title(article_content: str, output_language: str = 'en') ->
     Ensure the title grabs attention and would entice readers on Medium.com to click and read the story. The title should be creative and concise, ideally under 60 characters.
     Whenever possible, use one of the following formats: "How [Action|Benefit] WITHOUT [Pain Point]?", "How to [Action|Benefit] in [Limited Time]?", "The New Way to [Action] With No [Friction Point]", "Use/Adopt [Skill|Action] or [Consequence]".
 
-    Don't use irrelevant adjective like Unlock, Evolving, Embrace, Enrich, Unleash, Unmask, Unveil, Streamline, Fast-paced, Game-changer, ... and prioritize simple words.""",
+    Don't use irrelevant adjective like Unlock, Effortless, Evolving, Embrace, Enrich, Unleash, Unmask, Unveil, Streamline, Fast-paced, Game-changer, ... and prioritize simple words.""",
 
         'fr': f"""Tu es un expert en rédaction de contenu. À partir du contenu fourni ci-dessous, génère un titre accrocheur pour un article Medium.com.
 
